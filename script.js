0a94e7d8f97b930951945d8119cd36f8ca035999
Heewon Ahn <57425238+shelost@users.noreply.github.com>
2021-07-04 00:58:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,64 @@ let pos = 0
 
 let header = document.getElementsByClassName('header')[0]
 
-window.onscroll = () => {
-    if (document.documentElement.scrollTop > pos){
-        console.log('scrolling down')
-        header.classList.remove('active')
+
+
+let expos = document.getElementsByClassName('expo')
+let pfps = document.getElementsByClassName('pfp')
+
+
+let loop = () => {
+
+    if (window.innerWidth < 900){
+        for (let i=0; i<2; i++){
+            let index = i*2+1
+
+            let expo = expos[index]
+            let pfp = pfps[index]
+
+            expo.style.transform = `translateY(${pfp.getBoundingClientRect().height}px)`
+            pfp.style.transform = `translateY(-${expo.getBoundingClientRect().height}px)`
+        }
+
+        document.getElementById('bars').onclick = () => {
+            document.getElementsByClassName('header')[0].classList.toggle('active')
+        }
+
+        for (let i=0; i< document.getElementsByClassName('navbtn').length; i++){
+            let btn = document.getElementsByClassName('navbtn')[i]
+            btn.onclick = () => {
+                document.getElementsByClassName('header')[0].classList.remove('active')
+                console.log('yo')
+            }
+        }
+
     }else{
-        console.log('scrolling up')
-        header.classList.add('active')
+        for (let i=0; i<2; i++){
+            let index = i*2+1
+
+            let expo = expos[index]
+            let pfp = pfps[index]
+
+            expo.style.transform = ``
+            pfp.style.transform = ``
+        }
+
+        window.onscroll = () => {
+
+            if (document.documentElement.scrollTop > pos){
+                console.log('scrolling down')
+                header.classList.remove('active')
+            }else{
+                console.log('scrolling up')
+                header.classList.add('active')
+            }
+
+            pos = document.documentElement.scrollTop
+        }
     }
-    pos = document.documentElement.scrollTop
+
+    window.requestAnimationFrame(loop)
 }
+
+window.requestAnimationFrame(loop)
+
